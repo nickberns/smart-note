6bd957dc9673757032fed4a1d6240cf97b5c4d49
nickberns <nick@bluelightweb.co.nz>
2018-08-18 17:44:39 +1200

    all

---
+++ notes.js
@@ -0,0 +1,64 @@
+console.log('Starting notes.js');
+
+const fs = require('fs');
+
+var fetchNotes = () => {
+  try {
+    var notesString = fs.readFileSync('notes-data.json');
+    return JSON.parse(notesString);
+  } catch (e) {
+    return [];
+  }
+};
+
+var saveNotes = (notes) => {
+  fs.writeFileSync('notes-data.json', JSON.stringify(notes));
+};
+
+var addNote = (title, body) => {
+  var notes = fetchNotes();
+  var note = {
+    title,
+    body
+  };
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    saveNotes(notes);
+    return note;
+  }
+};
+
+var getAll = () => {
+  // debugger;
+  return fetchNotes();
+};
+
+var getNote = (title) => {
+  console.log('Getting note', title);
+  var notes = fetchNotes();
+  var singleNote = notes.filter((note) => note.title === title);
+  return singleNote[0];
+};
+
+var removeNote = (title) => {
+  console.log('Trying to Removing note:', title);
+  var notes = fetchNotes();
+  var filteredNotes = notes.filter((note) => note.title !== title);
+  saveNotes(filteredNotes);
+  return notes.length !== filteredNotes.length;
+};
+
+var logNote = (note) => {
+  console.log('-----');
+  console.log("title: " + note.title + " , body: " + note.body);
+};
+
+module.exports = {
+  addNote,
+  getAll,
+  getNote,
+  removeNote,
+  logNote
+};
